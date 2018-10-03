98040601af17214456cf802d9571cd0364100243
Lena Plotnikova <plotman@gmail.com>
2018-10-03 20:27:17 +0200

    Views added

--- app/controllers/motos_controller.rb
+++ app/controllers/motos_controller.rb
@@ -1,7 +1,18 @@
 class MotosController < ApplicationController
   def show
+    @moto = Moto.find(params[:id])
   end
 
   def index
+    @motos = Moto.all
+    if params[:query].present?
+      sql_query = " \
+      motos.brand LIKE :query \
+      OR motos.name LIKE :query \
+    "
+    @motos = @motos.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @motos = Moto.all
+    end
   end
 end
--- app/models/moto.rb
+++ app/models/moto.rb
@@ -1,3 +1,10 @@
 class Moto < ApplicationRecord
   belongs_to :user
+
+  include PgSearch
+  pg_search_scope :search_by_name_and_brand_and_moto_type,
+    against: [ :name, :brand, :moto_type ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
